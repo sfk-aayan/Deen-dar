6b93c31904894488d6733a3aa294fc97448c9146
Sajid Chowdhury <sajidahmed@iut-dhaka.edu>
2023-05-26 14:30:30 +0600

    Profile page more info added

---
+++ app/src/main/java/com/example/deen_dar/profile_setup.java
@@ -0,0 +1,14 @@
+package com.example.deen_dar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile_setup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_setup);
+    }
+}
