fdddbff28420d647ca6ada302d23d3cd071d1a14
Sajid Chowdhury <sajidahmed@iut-dhaka.edu>
2023-05-28 01:57:12 +0600

    cards made

---
+++ app/src/main/java/com/example/deen_dar/cards.java
@@ -0,0 +1,14 @@
+package com.example.deen_dar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards);
+    }
+}
