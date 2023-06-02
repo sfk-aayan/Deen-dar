8657f9230c1e7846c8b9886fdddb3665c1acdd1d
sfk-aayan <190041116faiyaz@gmail.com>
2023-06-03 03:37:18 +0600

    Added remaining nav bar

--- app/src/main/java/com/example/deen_dar/NoMoreMatch.java
+++ app/src/main/java/com/example/deen_dar/NoMoreMatch.java
@@ -2,13 +2,45 @@ package com.example.deen_dar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class NoMoreMatch extends AppCompatActivity {
+    private ImageView matchesImg, matchingImg, profileImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_more_match);
+        matchingImg = findViewById(R.id.imageView2);
+        matchesImg = findViewById(R.id.imageView9);
+        profileImg = findViewById(R.id.imageView10);
+
+        matchingImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NoMoreMatch.this, Cards.class);
+                startActivity(i);
+            }
+        });
+
+        profileImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NoMoreMatch.this, Profile.class);
+                startActivity(i);
+            }
+        });
+
+        matchesImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NoMoreMatch.this, Match.class);
+                startActivity(intent);
+            }
+        });
     }
 }
